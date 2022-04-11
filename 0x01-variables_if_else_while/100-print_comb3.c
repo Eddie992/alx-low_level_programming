403f85e2aea43c1f4f4fc7df32890d4f340c6849
Edward kataika <ekataika2002@gmail.com>
2022-04-12 01:26:03 +0200

    correct output

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - a simple program that outputs 00-99 separated by commas
+ * main - a simple program that outputs 0-9 separated by commas
  *
  * Return: 0 on success
  */
@@ -10,17 +10,19 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 48; i < 58; i++)
-		for (j = 48; j < 58; j++)
+	for (i = 48; i < 57; i++)
+	{
+		for (j = i + 1; j < 58; j++)
 		{
 			putchar(i);
 			putchar(j);
-			if (i != 57 || j != 57)
+			if (i != 56 || j != 57)
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
+	}
 	putchar('\n');
 	return (0);
 }
