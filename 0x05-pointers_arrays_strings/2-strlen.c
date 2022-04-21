318f0105a44070e8ecb24374a730e96ccc21e6fb
Edward kataika <ekataika2002@gmail.com>
2022-04-21 22:01:27 +0200

    character string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - get length of a string
+ *
+ * @s: string to get length of
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+strlen(My_first_strlen);
+}
