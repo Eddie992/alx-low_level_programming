05b9f5088c8ad0585b6fc36b9c585811b6947a61
Edward kataika <ekataika2002@gmail.com>
2022-04-21 22:28:07 +0200

    printa string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - puts a string followed by a newline to standard output
+ *
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+char *str;
+str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+puts(str);
+return (0);
+}
