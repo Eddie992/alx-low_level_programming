b62f630a1685ac92ca8bca8391c7b84afd3b3fed
Edward kataika <ekataika2002@gmail.com>
2022-04-28 20:55:11 +0200

    advanced fixes

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,60 +1,46 @@
-int wildmode(char *s1, char *s2);
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen_recursion - return length of string
+ *
+ * @s: string to get length of
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
 
 /**
- * exactmode - Goes to end of found substring
+ * palindrome_finder - finds palindromes
  *
- * @s1: non-wildcarded string we're checking for matches
- * @s2: wildcarded string we're checking the substring from
+ * @s: string to find palindrome of
+ * @l: last offset of string
  *
- * Return: continue wild mode at end of substring if
- * substring found, 0 otherwise
+ * Return: 1 if palindrome, 0 otherwise
  */
-int exactmode(char *s1, char *s2)
+int palindrome_finder(char *s, int l)
 {
-	if (*s2 == '*')
-		return (wildmode(s1, s2 + 1));
-	if (*s1 == 0 && *s2 == 0)
+	if (l < 1)
 		return (1);
-	if (*s1 == *s2)
-		return (exactmode(s1 + 1, s2 + 1));
+	if (*s == *(s + l))
+		return (palindrome_finder(s + 1, l - 2));
 	return (0);
 }
 
 /**
- * wildmode - Processes wildcards from s2 and handles backtracking
- * to match multiple instances of wildcarded substrings
+ * is_palindrome - returns if string is palindrome or not
  *
- * @s1: string we're trying to match
- * @s2: wildcarded string containing search pattern
+ * @s: string to test
  *
- * Return: 1 if valid match, 0 if not
+ * Return: 1 if palindrome, 0 otherwise
  */
-int wildmode(char *s1, char *s2)
+int is_palindrome(char *s)
 {
-	if (*s2 == '*')
-		return (wildmode(s1, s2 + 1));
-	if (*s2 == 0)
-		return (1);
-	if (*s1 == 0)
-		return (0);
-	if (*s1 != *s2)
-		return (wildmode(s1 + 1, s2));
-	if (!(exactmode(s1 + 1, s2 + 1)))
-		return (wildmode(s1 + 1, s2));
-	return (1);
-}
+	int len = _strlen_recursion(s);
 
-/**
- * wildcmp - Compares two strings with * wildcards. Sets initial search mode.
- *
- * @s1: first string, does not have *
- * @s2: second string, has *
- *
- * Return: 1 if identical, otherwise 0
- */
-int wildcmp(char *s1, char *s2)
-{
-	if (*s2 == '*')
-		return (wildmode(s1, s2));
-	return (exactmode(s1, s2));
+	return (palindrome_finder(s, len - 1));
 }
